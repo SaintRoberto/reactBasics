9a0650da94a7efe8d85b6a125f785d37b2fa62f9
David <luna.robertodev@gmail.com>
2020-11-14 14:23:02 -0500

    se agrega nuevo formulario

--- src/pages/EjerciciosNuevo.js
+++ src/pages/EjerciciosNuevo.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import ExerciseForm from '../components/ExerciseForm'
+import Card from '../components/Card'
+import { render } from '@testing-library/react'
 
 class EjerciciosNuevo extends React.Component {
-    render(){
-        return 'EjerciciosNuevo'
-    }
+   handleChange = e => {
+       this.setState({
+           [e.target.name]: e.target.value
+       })
+   }
+
+   render(){
+       return(
+           <ExerciseForm/>
+       )
+   }
 }
 
 export default EjerciciosNuevo
